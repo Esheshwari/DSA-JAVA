717f887425b7a6715256115eb100672807a85b89
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-06-22 18:39:57 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,6 @@
+public class Demo{
+    public static void main(String[] args) {
+        System.out.println(args[1]); // This will print the first command line argument passed to the program
+    }
+}
+//For output give input as Java Demo Esha Hello it will print 1st argument that is Hello if u will change args[1] to args[0] it will print Esha
