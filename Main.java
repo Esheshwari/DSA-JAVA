2c8be3bcc092ef45a62d6b7bfe9c2b49dc66062f
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-06-23 21:37:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@
 public class Main{   //Main is name of file if u will change name or main here it will give an error, This public means that this class will be accessible by everyone
     //function in classes r k/a method the below 2 line of code is method for class main
     public static void main(String[] args){ //This is the main method, the entry point of the program, string S will always be capital, void is return type wont return any value and static keyword means that the method or variable belongs to the class itself, not to any specific object created from that class
-        System.out.println("Hello, World!");              //This line prints "Hello, World!" to the console
+        System.out.println("Hello, World!");   
+        //system is a class  //This line prints "Hello, World!" to the console
     }                                                     //End of main method
 }
 //(String[] args) is command line variable, whatever value we will store, it will be stored here
