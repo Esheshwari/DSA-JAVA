9b5834c8ce31c464e6237d9b0bd31e1c8eda676b
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-06-24 20:44:49 +0530

    Create Output.java

---
+++ Output.java
@@ -0,0 +1,8 @@
+public class Output{
+    public static void main (String args[]){
+        System.out.println("Hello World");// ln will print it in diff line 
+        System.out.print("Hello World");
+        System.out.print("Hello World");//now this will be printed together
+        System.out.print("Hello World");
+    }
+}
