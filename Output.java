0bfe19f84caa6830987078813b7eebd36429dc27
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-06-24 21:05:35 +0530

    Update Output.java

--- Output.java
+++ Output.java
@@ -3,6 +3,12 @@ public class Output{
         System.out.println("Hello World");// ln will print it in diff line same canbe achieved with \n
         System.out.print("Hello World\n");
         System.out.print("Hello World");//now this will be printed together
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.print("I am esha. \n I am 19");
     }
 }
+
+
+//println is for 2 diff line/statement
+/*but \n works for same statement too 
+    ex: print("I am esha. \n I am 19")*/
