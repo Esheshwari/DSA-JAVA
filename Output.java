df9354b9590405dc7b17319b0a955a4bf2b87502
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-06-24 20:59:59 +0530

    Update Output.java

--- Output.java
+++ Output.java
@@ -1,7 +1,7 @@
 public class Output{
     public static void main (String args[]){
-        System.out.println("Hello World");// ln will print it in diff line 
-        System.out.print("Hello World");
+        System.out.println("Hello World");// ln will print it in diff line same canbe achieved with \n
+        System.out.print("Hello World\n");
         System.out.print("Hello World");//now this will be printed together
         System.out.print("Hello World");
     }
