5bc68bc16dece64ca5543b587d2b55e2707c1440
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-06-25 14:15:03 +0530

    practise1.java

---
+++ practise1.java
@@ -0,0 +1,14 @@
+/* print the given pattern
+*
+**
+***
+****
+ */
+public class practise1{
+    public static void main (String args[]){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
